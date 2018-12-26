d5ce3f2c336de0c7d8e97c241e59d45f0f76e143
b-pourya <pooria_beiki@yahoo.com>
2018-12-27 01:59:11 +0330

    fix instruction memory bugs

--- arm.v
+++ arm.v
@@ -33,8 +33,7 @@ end
 oc os0(clk);
 pc pc0(clk,pc_reset,1,newpc,oldpc);
 adder adder0(oldpc,64'b100,newpc);
-//memory memory0(oldpc,datain,1'b0,1'b1,clk,dataout);
-InstructionMemory instruction_memory (old_pc,instruction);
+InstructionMemory instruction_memory (oldpc,instruction);
 ControlUnit control_unit (instruction[31 : 21],reg_to_loc,branch,mem_read,mem_to_reg,Alu_Op,mem_write,alu_src,reg_write);
 multiplexer RB_multiplexer (instruction[20 : 16],instruction[4 : 0],reg_to_loc,output_register_bank_multiplexer);
 registerBank register_bank (instruction[9 : 5],instruction[4 : 0],output_register_bank_multiplexer,input_data_register,clk,reg_write,reg_data_1,reg_data_2);
@@ -42,7 +41,7 @@ multiplexer alu_multiplexer(reg_data_2,output_sign_extend,alu_src,output_alu_mul
 alu alu1 (reg_data_1,output_alu_multiplexer,alu_opcode,output_alu,zero_alu);
 ShiftUnit shift_unit (output_sign_extend,output_shift_unit);
 SignExtend sign_extend (instruction,output_sign_extend);
-adder shift_unit_adder(old_pc,output_shift_unit,output_shift_unit_adder);
+adder shift_unit_adder(oldpc,output_shift_unit,output_shift_unit_adder);
 multiplexer shift_unit_multiplexer (output_pc_adder,output_shift_unit_adder,branch & zero_alu,new_pc);
 Datamemory data_memory (output_alu,reg_data_2,mem_write,mem_read,clk,output_data_memory);
 multiplexer data_memory_multiplexer (output_data_memory,output_alu,mem_to_reg,input_data_register);
